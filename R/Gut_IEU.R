9cda3e9b46598fcbe953b419c3fdd8978b60850e
shaoming1995 <116997703+shaoming1995@users.noreply.github.com>
2023-12-26 15:58:36 +0800

    Add files via upload

---
+++ R/Gut_IEU.R
@@ -0,0 +1,128 @@
+#' @title 肠道菌群MR工具
+#' @param name 学号
+#' @param key 密码
+#' @param savefile 设置一个保存结果的文件夹名字
+#' @param PATH 切分的肠道菌群暴露文件位置
+#' @param GWASID 结局的GWAS id号
+#' @param kb 聚类的距离
+#' @param r2 相关系数
+#' @param outname 结局的英文名称或者缩写
+#' @export
+Gut_IEU<-function(name,key,savefile,PATH,GWASID,outname,kb,r2){
+  A <- name
+  A <- as.numeric(gsub("DK", "00", A))
+  C <- A + key
+  if (C == 2310000) {
+  library(TwoSampleMR)
+  dir.create(savefile)
+  filename<-data.frame(dir(PATH))
+  A_temp<-c()
+  B_temp<-c()
+  C_temp<-c()
+  D_temp <- c()
+  for (i in filename[,1]){
+    ipath<-paste0(PATH,"/",i)
+    exp_temp<-read.csv(ipath,header = T)
+    test2 <- (try(exp_temp1 <- clump_data(exp_temp, clump_kb = kb,
+                                          clump_r2 = r2)))
+    if (class(test2)!= "try-error") {
+      OUT<-extract_outcome_data(snps=exp_temp1$SNP,outcomes=GWASID,proxies=T,maf_threshold = 0.01,access_token = NULL)
+      if(dim(OUT)[[1]]!=0){
+        OUT$id.outcome<-outname
+        OUT$outcome<-outname
+        OUT<-OUT[!duplicated(OUT$SNP),]
+        exp_temp_out<-merge(exp_temp1,OUT,by="SNP",all=F)
+        exp_temp_out$eaf.exposure<-NA
+        #分别取出暴露与结局的数据
+        exp<- exp_temp_out[,c("SNP","effect_allele.exposure","other_allele.exposure","beta.exposure","se.exposure",
+                              "pval.exposure","id.exposure","exposure", "eaf.exposure")]
+        out<- exp_temp_out[,c("SNP","effect_allele.outcome","other_allele.outcome", "eaf.outcome", "beta.outcome","se.outcome",
+                              "pval.outcome","id.outcome","outcome","eaf.outcome")]
+        #调控方向
+        dat <-harmonise_data(exposure_dat=exp,outcome_dat=out,action=2)
+        res <- mr(dat)
+        data_h<-dat%>%subset(dat$mr_keep==TRUE)
+        data_h$Fvalue <- (data_h$beta.exposure/data_h$se.exposure)*(data_h$beta.exposure/data_h$se.exposure)
+        data_h_TableS1 <- data_h[, c("exposure","SNP","effect_allele.exposure", "other_allele.exposure",
+                                     "beta.exposure", "se.exposure","Fvalue","pval.exposure",
+                                     "beta.outcome","se.outcome", "pval.outcome")] #导出SNP表格用
+        res$cluster <- 1    #0表达富集成功
+        mr_OR<-generate_odds_ratios(res)
+        mr_OR$or<-round(mr_OR$or,3)
+        mr_OR$or_lci95<-round(mr_OR$or_lci95,3)
+        mr_OR$or_uci95 <- round(mr_OR$or_uci95,3)
+        mr_OR$OR_CI <- paste0(mr_OR$or,"(",mr_OR$or_lci95,"-",mr_OR$or_uci95,")")
+
+        het <- mr_heterogeneity(dat)
+        ple <- mr_pleiotropy_test(dat)
+        A_temp <- rbind(mr_OR, A_temp)
+        B_temp<-rbind(het,B_temp)
+        C_temp<-rbind(ple,C_temp)
+        D_temp <- rbind(data_h_TableS1, D_temp)
+
+        print(paste0("当前运行到",i,"文件"))
+        Aname<-paste0(savefile,"/","肠道菌群与",outname,"的MR结果.csv")
+        Bname<-paste0(savefile,"/","肠道菌群与",outname,"的异质性结果.csv")
+        Cname<-paste0(savefile,"/","肠道菌群与",outname,"的多效性结果.csv")
+        Dname <- paste0(savefile, "/", "肠道菌群与",
+                        outname, "的SNPs情况.csv")
+        write.csv(A_temp,Aname,row.names = F)
+        write.csv(B_temp,Bname,row.names = F)
+        write.csv(C_temp,Cname,row.names = F)
+        write.csv(D_temp, Dname, row.names = F)}
+      else{
+        cat(i,"与",outname,"未找到工具变量不进行计算")}}
+    else{
+      cat(i,"由于网络502问题未完成clump")
+      OUT<-extract_outcome_data(snps=exp_temp$SNP,outcomes=GWASID,proxies=T,maf_threshold = 0.01,access_token = NULL)
+      if(dim(OUT)[[1]]!=0){
+        OUT$id.outcome<-outname
+        OUT$outcome<-outname
+        OUT<-OUT[!duplicated(OUT$SNP),]
+        exp_temp_out<-merge(exp_temp,OUT,by="SNP",all=F)
+        exp_temp_out$eaf.exposure<-exp_temp_out$eaf.outcome
+        #分别取出暴露与结局的数据
+        exp<- exp_temp_out[,c("SNP","effect_allele.exposure","other_allele.exposure","beta.exposure","se.exposure",
+                              "pval.exposure","id.exposure","exposure", "eaf.exposure")]
+        out<- exp_temp_out[,c("SNP","effect_allele.outcome","other_allele.outcome", "eaf.outcome", "beta.outcome","se.outcome",
+                              "pval.outcome","id.outcome","outcome","eaf.outcome")]
+        #调控方向
+        dat <-harmonise_data(exposure_dat=exp,outcome_dat=out,action=2)
+        res <- mr(dat)
+        data_h<-dat%>%subset(dat$mr_keep==TRUE)
+        data_h$Fvalue <- (data_h$beta.exposure/data_h$se.exposure)*(data_h$beta.exposure/data_h$se.exposure)
+        data_h_TableS1 <- data_h[, c("exposure","SNP","effect_allele.exposure", "other_allele.exposure",
+                                     "beta.exposure", "se.exposure","Fvalue","pval.exposure",
+                                     "beta.outcome","se.outcome", "pval.outcome")] #导出SNP表格用
+        res$cluster <- 0    #0表达没有富集成功
+        mr_OR<-generate_odds_ratios(res)
+        mr_OR$or<-round(mr_OR$or,3)
+        mr_OR$or_lci95<-round(mr_OR$or_lci95,3)
+        mr_OR$or_uci95 <- round(mr_OR$or_uci95,3)
+        mr_OR$OR_CI <- paste0(mr_OR$or,"(",mr_OR$or_lci95,"-",mr_OR$or_uci95,")")
+        het <- mr_heterogeneity(dat)
+        ple <- mr_pleiotropy_test(dat)
+        A_temp <- rbind(mr_OR, A_temp)
+        B_temp<-rbind(het,B_temp)
+        C_temp<-rbind(ple,C_temp)
+        D_temp <- rbind(data_h_TableS1, D_temp)
+        print(paste0("当前运行到",i,"文件"))
+        Aname<-paste0(savefile,"/","肠道菌群与",outname,"的MR结果.csv")
+        Bname<-paste0(savefile,"/","肠道菌群与",outname,"的异质性结果.csv")
+        Cname<-paste0(savefile,"/","肠道菌群与",outname,"的多效性结果.csv")
+        Dname <- paste0(savefile, "/", "肠道菌群与",
+                        outname, "的SNPs情况.csv")
+        write.csv(A_temp,Aname,row.names = F)
+        write.csv(B_temp,Bname,row.names = F)
+        write.csv(C_temp,Cname,row.names = F)
+        write.csv(D_temp, Dname, row.names = F)}else{
+          cat(i,"与",outname,"未找到工具变量不进行计算")}
+    }
+  }
+  cat("当前分析已完成请前往肠道菌群MR结果文件夹下查看结果")
+  }
+  else {
+    cat("请联系客服获取账户学号和密码或微信联系SFM19950928")
+  }
+}
+
