70ad236c9840706ca40a8f238652d72ab316e821
shaoming1995 <116997703+shaoming1995@users.noreply.github.com>
2023-12-26 14:19:59 +0800

    Add files via upload

--- R/Find_nearest_gene.R
+++ R/Find_nearest_gene.R
@@ -4,22 +4,77 @@
 #' @param build 输入SNP参考的基因组位置，默认hg19，其他hg18或者hg38
 #' @param filename 输出结果的文件夹名称
 #' @export
-#' @examples
 Find_nearest_gene<-function(data,flanking = 0, build = "hg19",filename="匹配基因文件"){
-
+  library(dplyr)
   data1<-data[,c("SNP","chr.exposure","pos.exposure")]#染色体 位置 SNP
+  find_nearest_gene <-function(data, flanking=100, build='hg19', collapse=TRUE, snp='rsid', chr='chromosome', bp='position'){
+
+    data <- data # not sure this is needed
+
+    if(build == 'hg18'){
+      genelist <- hg18genelist
+    }
+
+    if(build == 'hg19'){
+      genelist <- hg19genelist
+    }
+
+    if(build == 'hg38'){
+      genelist <- hg38genelist
+    }
+
+    if(flanking != 100){
+      flanking = as.numeric(flanking)
+    }
+
+    if(snp != 'rsid'){
+      data <- data %>% rename(rsid = snp)
+    }
+
+    if(chr != 'chromosome'){
+      data <- data %>% rename(chromosome = chr)
+    }
+
+    if(bp != 'position'){
+      data <- data %>% rename(position = bp)
+    }
+
+    data<-sqldf::sqldf(sprintf("select A.*,B.* from
+              data A left join genelist B
+              ON (A.chromosome == B.CHR and
+              A.position >= B.START - %1$s and
+              A.position <= B.STOP + %1$s)", flanking*1000)
+    )
+    if (collapse==TRUE){
+      data %>%
+        dplyr::group_by(rsid, chromosome, position) %>%
+        dplyr::summarise(GENES = paste(GENE, collapse=',')) %>%
+        data.frame
+    } else {
+      data <- data %>%
+        dplyr::rename(geneSTART = START, geneSTOP = STOP) %>%
+        dplyr::select(rsid, chromosome, position, geneSTART,geneSTOP,GENE)
+
+      data$distance <- apply(data,1, FUN=function(x){
+        ifelse(
+          !is.na(x['GENE']) & x['position'] < x['geneSTART'], -(as.numeric(x['geneSTART']) - as.numeric(x['position'])),
+          ifelse(!is.na(x['GENE']) & x['position'] > x['geneSTOP'], as.numeric(x['position']) - as.numeric(x['geneSTOP']),
+                 ifelse(!is.na(x['GENE']) & (x['position'] > x['geneSTART']) & (x['position'] < x['geneSTOP']),'intergenic',NA))
+        )
+      })
+      data
+    }
+  }
   result<-find_nearest_gene(data1, flanking = flanking, build = build,
 
                             collapse = TRUE, snp = "SNP", chr = "chr.exposure",
 
                             bp = "pos.exposure")
 
-  D<-merge(result,data,by.x = "SNP",by.y="SNP",all = F)
+  D<-merge(data,result,by.x="SNP",by.y = "rsid",all = F)
   dir.create(filename)
   path<-paste0(getwd(),"/",filename,"/SNP_gene.csv")
   write.csv(D,path,row.names = F)
 }
-
-
 
 
