5fffff7730b2541ce23921754c4db7e6d28f039f
shaoming1995 <116997703+shaoming1995@users.noreply.github.com>
2024-04-03 21:42:49 +0800

    Add files via upload

--- R/Gut_local.R
+++ R/Gut_local.R
@@ -4,11 +4,12 @@
 #' @param savefile 设置一个保存结果的文件夹名字
 #' @param PATH 切分的肠道菌群暴露文件位置
 #' @param GWASsummay 本地结局的预处理好的GWAS summay
+#' @param local_clump 是否启动本地聚类,默认不启动
 #' @param kb 聚类距离
 #' @param r2 相关系数
 #' @param outname 本地结局的名称
 #' @export
-Gut_local<-function(name,key,savefile,PATH,GWASsummay,outname,kb,r2){
+Gut_local<-function(name,key,savefile,PATH,GWASsummay,outname,local_clump=F,kb,r2){
   A <- name
   A <- as.numeric(gsub("DK", "00", A))
   C <- A + key
@@ -23,8 +24,110 @@ Gut_local<-function(name,key,savefile,PATH,GWASsummay,outname,kb,r2){
     for (i in filename[, 1]) {
       ipath <- paste0(PATH, "/", i)
       exp_temp <- read.csv(ipath, header = T)
+      if(local_clump==F){
       test2 <- (try(exp_temp <- clump_data(exp_temp, clump_kb = kb,
-                                           clump_r2 = r2)))
+                                           clump_r2 = r2)))}else{
+                                             local_clump_data1<-function(temp_dat,pop,clump_kb,clump_r2){
+                                               temp_dat$rsid <- temp_dat$SNP
+                                               temp_dat$id <- temp_dat$id.exposure
+                                               temp_dat$pval <- temp_dat$pval.exposure
+                                               ld_sofm1<- function (dat, clump_kb, clump_r2, clump_p, bfile, plink_bin)
+                                               {
+                                                 shell <- ifelse(Sys.info()["sysname"] == "Windows", "cmd",
+                                                                 "sh")
+                                                 fn <- tempfile()
+                                                 write.table(data.frame(SNP = dat[["rsid"]], P = dat[["pval"]]),
+                                                             file = fn, row.names = F, col.names = T, quote = F)
+                                                 fun2 <- paste0(shQuote(plink_bin, type = shell), " --bfile ",
+                                                                shQuote(bfile, type = shell), " --clump ", shQuote(fn,
+                                                                                                                   type = shell), " --clump-p1 ", clump_p, " --clump-r2 ",
+                                                                clump_r2, " --clump-kb ", clump_kb, " --threads 20 --out ", shQuote(fn,
+                                                                                                                                    type = shell))
+                                                 system(fun2)
+                                                 res <- read.table(paste(fn, ".clumps", sep = ""), header = F)
+                                                 unlink(paste(fn, "*", sep = ""))
+                                                 y <- subset(dat, !dat[["rsid"]] %in% res[["V3"]])
+                                                 if (nrow(y) > 0) {
+                                                   message("Removing ", length(y[["rsid"]]), " of ", nrow(dat),
+                                                           " variants due to LD with other variants or absence from LD reference panel")
+                                                 }
+                                                 return(subset(dat, dat[["rsid"]] %in% res[["V3"]]))
+                                               }
+
+                                               ld_sofm2<-function (dat = NULL, clump_kb = 10000, clump_r2 = 0.001, clump_p = 0.99,
+                                                                   pop = "EUR", access_token = NULL, bfile = NULL, plink_bin = NULL)
+                                               {
+                                                 stopifnot("rsid" %in% names(dat))
+                                                 stopifnot(is.data.frame(dat))
+                                                 if (is.null(bfile)) {
+                                                   message("Please look at vignettes for options on running this locally if you need to run many instances of this command.")
+                                                 }
+                                                 if (!"pval" %in% names(dat)) {
+                                                   if ("p" %in% names(dat)) {
+                                                     warning("No 'pval' column found in dat object. Using 'p' column.")
+                                                     dat[["pval"]] <- dat[["p"]]
+                                                   }
+                                                   else {
+                                                     warning("No 'pval' column found in dat object. Setting p-values for all SNPs to clump_p parameter.")
+                                                     dat[["pval"]] <- clump_p
+                                                   }
+                                                 }
+                                                 if (!"id" %in% names(dat)) {
+                                                   dat$id <- random_string(1)
+                                                 }
+                                                 if (is.null(bfile)) {
+                                                   access_token = check_access_token()
+                                                 }
+                                                 ids <- unique(dat[["id"]])
+                                                 res <- list()
+                                                 for (i in 1:length(ids)) {
+                                                   x <- subset(dat, dat[["id"]] == ids[i])
+                                                   if (nrow(x) == 1) {
+                                                     message("Only one SNP for ", ids[i])
+                                                     res[[i]] <- x
+                                                   }
+                                                   else {
+                                                     message("Clumping ", ids[i], ", ", nrow(x), " variants, using ",
+                                                             pop, " population reference")
+                                                     if (is.null(bfile)) {
+                                                       res[[i]] <- ld_clump_api(x, clump_kb = clump_kb,
+                                                                                clump_r2 = clump_r2, clump_p = clump_p, pop = pop,
+                                                                                access_token = access_token)
+                                                     }
+                                                     else {
+                                                       res[[i]] <- ld_sofm1(x, clump_kb = clump_kb,
+                                                                            clump_r2 = clump_r2, clump_p = clump_p, bfile = bfile,
+                                                                            plink_bin = plink_bin)
+                                                     }
+                                                   }
+                                                 }
+                                                 res <- dplyr::bind_rows(res)
+                                                 return(res)
+                                               }
+                                               filepath1<-paste0(getwd(),"/1kg.v3/",pop)# /home/Refrence/EUR/ 改成你参考文件的路径
+                                               filepath2<-paste0(getwd(),"/1kg.v3/plink2_win64_20231212/plink2")
+                                               filepath3<-paste0(getwd(),"/1kg.v3/plink2_mac_20231212/plink2")
+                                               if(Sys.info()["sysname"] == "Windows"){
+                                                 temp_dat <- ld_sofm2(temp_dat,
+                                                                      plink_bin = filepath2, # /home/plink2.0/plink2改成你plink文件的路径
+                                                                      bfile = filepath1,
+                                                                      clump_kb = clump_kb, clump_r2 = clump_r2,pop=pop)}else{
+                                                                        temp_dat <- ld_sofm2(temp_dat,
+                                                                                             plink_bin = filepath3, # /home/plink2.0/plink2改成你plink文件的路径
+                                                                                             bfile = filepath1,
+                                                                                             clump_kb = clump_kb, clump_r2 = clump_r2,pop=pop)
+
+                                                                      }
+
+                                               temp_dat$rsid <- NULL
+                                               temp_dat$pval <- NULL
+                                               return(temp_dat)
+                                             }
+
+                                             test2 <- (try(exp_temp <- local_clump_data(exp_temp, clump_kb = kb,
+                                                                                  clump_r2 = r2)))
+                                           }
+
       if (class(test2) == "try-error") {
         cat(i,"由于网络502问题未完成clump")
         total <- merge(GWASsummay, exp_temp, by = "SNP")
