3d4f5a0af4a4daf93742efd36e28973246238912
shaoming1995 <116997703+shaoming1995@users.noreply.github.com>
2024-06-15 15:02:58 +0800

    Update IEU_1_to_local_3.R

--- R/IEU_1_to_local_3.R
+++ R/IEU_1_to_local_3.R
@@ -11,9 +11,11 @@
 #' @param data4GWAS 来自非IEU暴露4的GWAS summary数据文件
 #' @export
 IEU_1_to_local_3<-function(keyssh,data1IV,GWASID,data2IV,data2GWAS,data3IV,data3GWAS,data4IV,data4GWAS){
-  if (!require(tidyfst)) install.packages("tidyfst")
-  library(tidyfst)
-  if (Sys.info()["nodename"] == keyssh) {
+   library(tidyr)
+  RegistID_dat <- RegistID_dat
+  RegistID_u <- subset(RegistID_dat, IK == keyssh)
+  tempid <- paste0(keyssh, "_", Sys.info()["nodename"], "_",RegistID_u$RegistID)
+  if (RegistID_u$FINN %in% tempid) {
   #假设data1IV的暴露来自IEU data2IV和data2IV的暴露来自非IEU
   exp_name<-c("SNP","effect_allele.exposure","other_allele.exposure", "eaf.exposure", "beta.exposure","se.exposure", "pval.exposure","id.exposure","exposure")
   out_name<-c("SNP","effect_allele.outcome","other_allele.outcome", "eaf.outcome", "beta.outcome","se.outcome","pval.outcome","id.outcome","outcome")
