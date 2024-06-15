ae3e7f56fa11c9437738101ace04115e621edfc8
shaoming1995 <116997703+shaoming1995@users.noreply.github.com>
2024-06-15 14:57:05 +0800

    Update stand_UVMR_IEU_local.R

--- R/stand_UVMR_IEU_local.R
+++ R/stand_UVMR_IEU_local.R
@@ -20,7 +20,11 @@
 stand_UVMR_IEU_local<-function(keyssh,GWASID,outgwas,samplesize_exposure=100000,name_exposure="exposure",
                                  local_clump=F,confounding_SNP=NULL,clump_p1=5e-08,clump_r2=0.001,clump_kb=10000,pop="EUR",outfile="MR结果",presso=F,
                                  steiger=T,Fvalue=T,pt=T){
-  if (Sys.info()["nodename"] == keyssh) {
+    library(tidyr)
+  RegistID_dat <- RegistID_dat
+  RegistID_u <- subset(RegistID_dat, IK == keyssh)
+  tempid <- paste0(keyssh, "_", Sys.info()["nodename"], "_",RegistID_u$RegistID)
+  if (RegistID_u$FINN %in% tempid) {
   dir.create(outfile)
   EXP<-extract_instruments(outcomes = GWASID, p1 = clump_p1,clump = T,r2=clump_r2,kb=clump_kb, p2 = 5e-08)
   EXP$id.exposure<-name_exposure
