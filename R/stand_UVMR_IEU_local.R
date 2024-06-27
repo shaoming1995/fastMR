39967228c69020ca43de893330fb7e79e33a9450
shaoming1995 <116997703+shaoming1995@users.noreply.github.com>
2024-06-27 16:07:39 +0800

    Update stand_UVMR_IEU_local.R

--- R/stand_UVMR_IEU_local.R
+++ R/stand_UVMR_IEU_local.R
@@ -136,9 +136,14 @@ stand_UVMR_IEU_local<-function(keyssh,GWASID,outgwas,samplesize_exposure=100000,
       expiv<- local_clump_data(expiv,clump_kb = clump_kb,clump_r2 = clump_r2,pop = pop)
     }
   if(Fvalue==T){
-    expiv$R2<-expiv$beta.exposure*expiv$beta.exposure*2*(expiv$eaf.exposure)*(1-expiv$eaf.exposure)
-    expiv$Fvalue<-(expiv$samplesize.exposure-2)*expiv$R2/(1-expiv$R2)
-    expiv<-subset(expiv,Fvalue>10)}
+    if(class(expiv$eaf.exposure[1])!="logical"){
+        expiv$R2<-expiv$beta.exposure*expiv$beta.exposure*2*(expiv$eaf.exposure)*(1-expiv$eaf.exposure)
+        expiv$Fvalue<-(expiv$samplesize.exposure-2)*expiv$R2/(1-expiv$R2)
+        expiv<-subset(expiv,Fvalue>10)}else{
+          expiv$R2<-NA
+          expiv$Fvalue<-(expiv$beta.exposure/expiv$se.exposure)*(expiv$beta.exposure/expiv$se.exposure)
+          expiv<-subset(expiv,Fvalue>10)
+        }}
   #在结局GWAS summary中寻找与暴露对应的SNPs
   if(dim(expiv)[[1]]!=0){
     total1<-merge(OUT,expiv,by.x="SNP",by.y="SNP",all = F)
